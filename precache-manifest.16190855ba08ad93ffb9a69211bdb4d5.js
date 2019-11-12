bafa69e813f61deaf86283a5cf785144dc6bdf25
Villione <dmi.villione@gmail.com>
2019-11-12 12:55:15 +0300

    Updates

---
+++ precache-manifest.16190855ba08ad93ffb9a69211bdb4d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65661dbc2f73f6ed5a2c2cd4e811739",
+    "url": "/mohome-front/index.html"
+  },
+  {
+    "revision": "745ba51d1ed08a72f612",
+    "url": "/mohome-front/static/css/main.68ea5766.chunk.css"
+  },
+  {
+    "revision": "e0bce4aa92ef71a765fa",
+    "url": "/mohome-front/static/js/2.f696ec05.chunk.js"
+  },
+  {
+    "revision": "745ba51d1ed08a72f612",
+    "url": "/mohome-front/static/js/main.5dfacd7b.chunk.js"
+  },
+  {
+    "revision": "5cc46f9f47dc048c7715",
+    "url": "/mohome-front/static/js/runtime-main.abe6d2f3.js"
+  },
+  {
+    "revision": "0f7353a06f7ed1056f38e0ace19d555e",
+    "url": "/mohome-front/static/media/brand_blue.0f7353a0.png"
+  },
+  {
+    "revision": "0ef4e888dcf2d5fa227b60a3bceaffa2",
+    "url": "/mohome-front/static/media/music_placeholder.0ef4e888.svg"
+  },
+  {
+    "revision": "ebb108df2bce906a9345bde5511e7332",
+    "url": "/mohome-front/static/media/validationSpinner.ebb108df.gif"
+  }
+]);
